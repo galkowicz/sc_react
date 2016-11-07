14020999e30a789a0abb9a57b91e0870a388646c
orian.galkowicz <orian.galk@gmail.com>
2016-11-07 19:34:13 +0200

    first commit

---
+++ app/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var App = React.createClass({
+   render: function () {
+       return (
+       <div>
+           <h1>Test Orian 42222</h1>
+       </div>
+       )
+   }
+});
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+// import HTMLWebpackPlugin from 'html-webpack-plugin';
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject:'body'
+});
+
+module.exports = {
+
+    entry: __dirname + '/app/index.js',
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+
+    output: {
+        filename: 'transformed.js',
+        path: __dirname + '/dist'
+    },
+
+    plugins:[HTMLWebpackPluginConfig],
+
+    devServer: {
+        inline:true,
+        port: 9002
+    },
+};
